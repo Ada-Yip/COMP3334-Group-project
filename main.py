307f2ebd80ddf648e00d1ca3a80f27cab554eee7
Hugo <hugo.chau@bibo.email>
2026-03-18 17:42:37 +0800

    hot fix

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 """
 main entry point
 """
-from client.app import main as client_run
 from config import SERVER_HOST, SERVER_PORT
 import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run("server.api_server:app", host=SERVER_HOST, port=SERVER_PORT, reload=True)
-    client_run()
